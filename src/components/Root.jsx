432b72d74ad5209ae2743e1dc3030c0c7720ea46
Ian <DrilonIan@yahoo.com>
2020-06-21 21:27:16 +0800

    Initial commit

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,23 +1,37 @@
-import React, {lazy, Suspense} from 'react';
+import React from 'react';
 //Components
-const Home = lazy(() => import('../page/Home'));
-const NavigationComponent = lazy(() => import('./NavigationComponent'));
-const ChurchComponent = lazy(() => import('./ChurchComponent'));
-const MusicComponent = lazy(() => import('./MusicComponent'));
-const MinistriesComponent = lazy(() => import('./MinistriesComponent'));
-const EventsComponent = lazy(() => import('./EventsComponent'));
-const QuoteComponent = lazy(() => import('./QuoteComponent'));
-const LeadersComponent = lazy(() => import('./LeadersComponent'));
-const InformationComponent = lazy(() => import('./InformationComponent'));
-const InformationCardComponent = lazy(() => import('./InformationCardComponent'));
-const ServiceComponent = lazy(() => import('./ServiceComponent'));
-const UpComingEventsComponent = lazy(() => import('./UpComingEventsComponent'));
-const TestimonyComponent = lazy(() => import('./TestimonyComponent'));
-const FooterComponent = lazy(() => import('./FooterComponent'));
+import Home from '../page/Home';
+import NavigationComponent from './NavigationComponent';
+import ChurchComponent from './ChurchComponent';
+import MusicComponent from './MusicComponent';
+import MinistriesComponent from './MinistriesComponent';
+import EventsComponent from './EventsComponent';
+import QuoteComponent from './QuoteComponent';
+import LeadersComponent from './QuoteComponent';
+import InformationComponent from './QuoteComponent';
+import InformationCardComponent from './QuoteComponent';
+import ServiceComponent from './QuoteComponent';
+import UpComingEventsComponent from './QuoteComponent';
+import TestimonyComponent from './QuoteComponent';
+import FooterComponent from './QuoteComponent';
+// const Home = lazy(() => import('../page/Home'));
+// const NavigationComponent = lazy(() => import('./NavigationComponent'));
+// const ChurchComponent = lazy(() => import('./ChurchComponent'));
+// const MusicComponent = lazy(() => import('./MusicComponent'));
+// const MinistriesComponent = lazy(() => import('./MinistriesComponent'));
+// const EventsComponent = lazy(() => import('./EventsComponent'));
+// const QuoteComponent = lazy(() => import('./QuoteComponent'));
+// const LeadersComponent = lazy(() => import('./LeadersComponent'));
+// const InformationComponent = lazy(() => import('./InformationComponent'));
+// const InformationCardComponent = lazy(() => import('./InformationCardComponent'));
+// const ServiceComponent = lazy(() => import('./ServiceComponent'));
+// const UpComingEventsComponent = lazy(() => import('./UpComingEventsComponent'));
+// const TestimonyComponent = lazy(() => import('./TestimonyComponent'));
+// const FooterComponent = lazy(() => import('./FooterComponent'));
 class Root extends React.Component {
     render(){
         return(
-                <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
+               <>
                     <Home />
                     <NavigationComponent />
                     <ChurchComponent/>
@@ -32,7 +46,7 @@ class Root extends React.Component {
                     <TestimonyComponent/>
                     <UpComingEventsComponent/>
                     <FooterComponent />
-                </Suspense>
+                </>
         )
     }
 }
