42fbc8e10d8cbf34bcf773dc0ad6658ea899a26f
Ian <DrilonIan@yahoo.com>
2020-06-21 20:55:26 +0800

    lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-//components
-import Root from './components/Root'
-import Church from './page/Church'
-import Music from './page/Music'
+
 import Error from './page/404'
 import ScrollToTop from './utils/ScrollToTop';
-
+//components
+const Root = lazy(() => import('./components/Root'));
+const Church = lazy(() => import('./page/Church'));
+const Music = lazy(() => import('./page/Music'));
 class App extends React.Component {
   render(){
     return (
       <Router>
         <ScrollToTop>
-        <Switch>
-            <Route exact path="/" component={Root}/>
-            <Route exact path="/church" component={Church}/>
-            <Route exact path="/music" component={Music}/>
-            <Route exact component={Error}/>
-        </Switch>
+        <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
+          <Switch>
+              <Route exact path="/" component={Root}/>
+              <Route exact path="/church" component={Church}/>
+              <Route exact path="/music" component={Music}/>
+              <Route exact component={Error}/>
+          </Switch>
+        </Suspense>
         </ScrollToTop>
       </Router>
     );
